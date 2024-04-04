b18ba986391511d33fde6c64cfc81aa6fe15cf20
Firas Lafi <flafi@3-G-10.42heilbronn.de>
2024-04-04 02:15:59 +0200

    tpt ready

--- build/src/parsing/pad.d
+++
@@ -1,7 +0,0 @@
-build/src/parsing/pad.o: src/parsing/pad.c include/../include/hud.h \
-  include/../include/cub_3d.h include/../include/parser.h \
-  include/../include/raycast.h include/../include/utils.h \
-  include/../libs/MLX42/include/MLX42/MLX42.h \
-  include/../libs/libft/fts.h libs/libft/./libft/libft.h \
-  libs/libft/./gnl/get_next_line.h libs/libft/./ft_printf/ft_printf.h \
-  libs/libft/./ft_printf/../libft/libft.h
